ef3c55851a50e3fbbb693312e5266043d12e4b39
Никита <nikit.suvorov@gmail.com>
2020-03-31 20:58:14 +0300

    структура папок

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import './App.css';
-import TodoListHeader from "./TodoListHeader";
-import TodoListTasks from "./TodoListTasks";
-import TodoListFooter from "./TodoListFooter ";
+import TodoListHeader from "./components/TodoListHeader/TodoListHeader";
+import TodoListTasks from "./components/TodoListTasks/TodoListTasks";
+import TodoListFooter from "./components/TodoListFooter/TodoListFooter";
 
 class App extends React.Component {
     render = () => {
         return (
             <div className="App">
                 <div className="todoList">
-                    <TodoListHeader />
-                    <TodoListTasks />
-                    <TodoListFooter />
+                    <TodoListHeader/>
+                    <TodoListTasks/>
+                    <TodoListFooter/>
                 </div>
             </div>
         );
--- src/components/TodoListFooter/TodoListFooter.js
+++ src/components/TodoListFooter/TodoListFooter.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-
 class TodoListFooter extends React.Component {
     render = () => {
         return (
