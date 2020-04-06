b2746051877fb7525ba2a038f48b0b7f4a673089
Никита <nikit.suvorov@gmail.com>
2020-04-06 21:21:03 +0300

    part2

--- src/App.js
+++ src/App.js
@@ -5,13 +5,24 @@ import TodoListTasks from "./components/TodoListTasks/TodoListTasks";
 import TodoListFooter from "./components/TodoListFooter/TodoListFooter";
 
 class App extends React.Component {
+
+    tasks = [
+        {title: "CSS", isDone: true, priority: "low"},
+        {title: "JS", isDone: false, priority: "height"},
+        {title: "ReactJS", isDone: false, priority: "low"},
+        {title: "Patterns", isDone: true, priority: "low"}
+    ];
+
+    filterValue = "Completed";
+
     render = () => {
+
         return (
             <div className="App">
                 <div className="todoList">
                     <TodoListHeader/>
-                    <TodoListTasks/>
-                    <TodoListFooter/>
+                    <TodoListTasks tasks={this.tasks}/>
+                    <TodoListFooter filterValue={this.filterValue}/>
                 </div>
             </div>
         );
